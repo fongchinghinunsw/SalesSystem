96b8c018a6cb2e2109fa32504d55096e81d7d487
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-27 06:26:29 +0000

    [PS2] accounts: removed unused module, correct indent.
    Change-Id: I0e63c7cbd0338ff62377dcafe2858736129a6b42

--- user.py
+++ user.py
@@ -1,19 +1,12 @@
-import bcrypt
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    user_type = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(60))
+    user_type = db.Column(db.Integer)
     
     def __repr__(self):
-        return f"User('{self.username}', '{self.username}')"
-    
+        return f"User('{self.id}', '{self.username}')"
