d449413d7e2789c9c261c6e4b203928aea5a129a
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-27 06:45:24 +0000

    [PS3] accounts: add user class.
    Change-Id: I0e63c7cbd0338ff62377dcafe2858736129a6b42

--- user.py
+++ user.py
@@ -3,10 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(60))
-    user_type = db.Column(db.Integer)
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(100), unique=True)
+  password = db.Column(db.String(60))
+  user_type = db.Column(db.Integer)
     
-    def __repr__(self):
-        return f"User('{self.id}', '{self.username}')"
+  def __repr__(self):
+    return f"User('{self.id}', '{self.username}')"
+        
+  def getUserID():
+    return id
+      
+  def getEmail():
+    return email
+      
+  def getType():
+    return user_type
