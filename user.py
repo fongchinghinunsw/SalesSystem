9512c3e24a8fc92ef7ee2f9d8ac4256fc7110ffd
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-28 10:49:42 +0000

    [PS7] accounts: add user class.
    Change-Id: I0e63c7cbd0338ff62377dcafe2858736129a6b42

--- user.py
+++ user.py
@@ -7,35 +7,31 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(100), unique=True)
   password = db.Column(db.String(60))
-  hashed_pwd = db.Column(db.String(60))
   user_type = db.Column(db.Integer)
 
   def __repr__(self):
     return f"User({self.id}, {self.email}, password, {self.user_type})"
 
-  def getUserID():
+  def getUserID(self):
     """Return an integer which is the user's ID"""
     return self.id
 
-  def getEmail():
+  def getEmail(self):
     """Return a string which is the user's email"""
     return self.email
 
-  def getType():
+  def getType(self):
     """Return 1 or 2 such that 1 means the user is a normal user, 2 
       means the user is a staff"""
     return self.user_type
 
-  def setPassword(password):
-    """Set a new password for the user, a hashed password with a randomly-
-      generated salt will then be generated and save as self.hashed_pwd"""
-    self.password = b"{0}".format(password)
-    self.hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
+  def setPassword(self, password):
+    """A hashed password with a randomly generated salt will be generated
+    and save in self.password"""
+    new_password = b"{0}".format(password)
+    self.password = bcrypt.hashpw(new_password, bcrypt.gensalt())
 
-  def verifyPassword(password):
+  def verifyPassword(self, password):
     """Verify the password by comparing the user input password and the hashed
-      password, if the password is correct return True, else return False"""
-    if bcrypt.checkpw(password, self.hashed_pwd):
-        return True
-    else:
-        return False
+    password, if the password is correct return True, else return False"""
+    return bcrypt.checkpw(password, self.hashed_pwd)
