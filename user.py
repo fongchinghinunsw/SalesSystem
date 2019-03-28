25f6408fd155bb181269b9d225f4dca085e61caf
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-28 05:59:34 +0000

    [PS5] accounts: add user class.
    Change-Id: I0e63c7cbd0338ff62377dcafe2858736129a6b42

--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+from bcrypt import hashpw, gensalt, checkpw
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,6 +7,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(100), unique=True)
   password = db.Column(db.String(60))
+  hashed_pwd = db.Column(db.String(60))
   user_type = db.Column(db.Integer)
 
   def __repr__(self):
@@ -16,9 +18,16 @@ class User(db.Model):
 
   def getEmail():
     return self.email
-   
+
   def getType():
     return self.user_type
-  
+
   def setPassword(password):
-    self.password = password
+    self.password = b"{0}".format(password)
+    self.hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
+
+  def verifyPassword(password):
+    if bcrypt.checkpw(password, self.hashed_pwd):
+        return True
+    else:
+        return False
