18e8130b6ea40ca8ec440201f636ae404d287655
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-28 10:25:11 +0000

    [PS6] accounts: add user class.
    Change-Id: I0e63c7cbd0338ff62377dcafe2858736129a6b42

--- user.py
+++ user.py
@@ -14,19 +14,27 @@ class User(db.Model):
     return f"User({self.id}, {self.email}, password, {self.user_type})"
 
   def getUserID():
+    """Return an integer which is the user's ID"""
     return self.id
 
   def getEmail():
+    """Return a string which is the user's email"""
     return self.email
 
   def getType():
+    """Return 1 or 2 such that 1 means the user is a normal user, 2 
+      means the user is a staff"""
     return self.user_type
 
   def setPassword(password):
+    """Set a new password for the user, a hashed password with a randomly-
+      generated salt will then be generated and save as self.hashed_pwd"""
     self.password = b"{0}".format(password)
     self.hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt())
 
   def verifyPassword(password):
+    """Verify the password by comparing the user input password and the hashed
+      password, if the password is correct return True, else return False"""
     if bcrypt.checkpw(password, self.hashed_pwd):
         return True
     else:
