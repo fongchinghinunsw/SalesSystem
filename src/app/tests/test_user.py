66cd39c848bec34c575893e254323bc159f70a26
Stephen Fong <fongchinghinunsw@gmail.com>
2019-03-31 10:40:07 +0000

    [PS3] accounts: add password length verification for user class
    Change-Id: I819d50dfaa6500cc6761b3a7be6a0f9f8c18b625

--- src/app/tests/test_user.py
+++ src/app/tests/test_user.py
@@ -1,4 +1,5 @@
 """Module to test the user model class"""
+import bcrypt
 import pytest
 from app.core.models.user import User
 
@@ -11,13 +12,23 @@ def test_user_password():
   assert user.VerifyPassword("test_pwd")
   assert not user.VerifyPassword("wrong_pwd")
   assert not user.VerifyPassword(None)
-  
+
 def test_invalid_length_for_new_password():
   """ Test new password with invalid length (< 4 or > 20)
   provided by user
   """
   user = User(email="test@foo.bar", user_type=0)
+  user_password = "ILoveHTML"
+  user.SetPassword(user_password)
+
+  new_password1 = "pwd"
   with pytest.raises(ValueError):
-    user.SetPassword("pwd")
+    user.SetPassword(new_password1)
+  assert(user.VerifyPassword(new_password1) == False)
+  assert(user.VerifyPassword(user_password))
+
+  new_password2 = "I love meatball and tuna."
   with pytest.raises(ValueError):
-    user.SetPassword("I love meatball and tuna.")
+    user.SetPassword(new_password2)
+  assert(user.VerifyPassword(new_password2) == False)
+  assert(user.VerifyPassword(user_password))
