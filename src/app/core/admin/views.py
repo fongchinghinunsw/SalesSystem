e6d1d09f17e5556332bb5f0a8529c09366bdd079
Qingyi Zhang <2392506080@qq.com>
2019-04-02 13:17:25 +0000

    [PS2] admin: implement order list
    Change-Id: I54dcfa6df92fac76b293212d4ba77bbfbd2de54b

--- src/app/core/admin/views.py
+++ src/app/core/admin/views.py
@@ -2,8 +2,14 @@
 
 from flask import render_template
 from . import bp as app  # Note that app = blueprint, current_app = flask context
+from app.core.models.order import Order
+from flask import render_template
 
 
 @app.route("/")
 def home():
   return render_template('admin/landing.html')
+
+@app.route("/order_list", methods = ['GET', 'POST'])
+def order_list():
+  return render_template("admin/order.html")
