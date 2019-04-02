9dd7796f01f0fbb5fc090a55db03d9f347887e38
Qingyi Zhang <2392506080@qq.com>
2019-04-02 13:10:59 +0000

    [PS1] core:implement order list
    Change-Id: I54dcfa6df92fac76b293212d4ba77bbfbd2de54b

--- src/app/core/admin/views.py
+++ src/app/core/admin/views.py
@@ -1,8 +1,9 @@
 """Admin blueprint views"""
 
 from . import bp as app  # Note that app = blueprint, current_app = flask context
+from app.core.models.order import Order
+from flask import render_template
 
-
-@app.route("/")
-def home():
-  return "Admin Routes - hello world"
+@app.route("/order_list", methods = ['GET', 'POST'])
+def order_list():
+  return render_template("admin/order.html")
