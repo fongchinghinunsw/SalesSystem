9da7e0c68aa90e795054eb5c12b1e504430d9b2a
Qingyi Zhang <2392506080@qq.com>
2019-04-04 11:45:37 +0000

    [PS6] admin: implement order list and tests
    Change-Id: I54dcfa6df92fac76b293212d4ba77bbfbd2de54b

--- src/app/tests/test_admin.py
+++ src/app/tests/test_admin.py
@@ -8,3 +8,14 @@ def test_index(client):
   """
   response = client.get('/admin/')
   assert b"Hello this is admin" in response.data
+
+def test_order_list(client):
+  response = client.get('/admin/order/')
+  rsp = str(response.data)
+  assert "id" in rsp
+  assert "user_name" in rsp
+  assert "price" in rsp
+  assert "create_at" in rsp
+  assert "updated_at" in rsp
+  assert "status" in rsp
+>>>>>>> admin:tests of displaying order list
