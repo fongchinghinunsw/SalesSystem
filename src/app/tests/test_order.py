de5536eb6bacd1a291d2f8336a6507fe515b24aa
Adam Yi <i@adamyi.com>
2019-04-04 11:54:51 +0000

    [PS7] core: implement order logic
    Change-Id: I66d444db18fe2bd9d27466977163335a2befb6a4

---
+++ src/app/tests/test_order.py
@@ -0,0 +1,39 @@
+"""Module to test the order model module"""
+import pytest
+from app.core.models.inventory import Stock, Item, IngredientGroup
+from app.core.models.order import Order
+from app.core.models import db
+
+
+def test_create_order(app):
+  """Test creating order and adding items to it"""
+  with app.app_context():
+    sburger = Stock(name="burger", amount=3)
+    swrap = Stock(name="wrap", amount=0)
+    db.session.add(sburger)
+    db.session.add(swrap)
+    imain = Item(name="main")
+    iburger = Item(name="burger")
+    iwrap = Item(name="wrap")
+    gtype = IngredientGroup(
+        name="type", min_item=1, max_item=1, min_option=1, max_option=1)
+    db.session.add(imain)
+    db.session.add(iburger)
+    db.session.add(iwrap)
+    db.session.add(gtype)
+    sburger.items.append(iburger)
+    swrap.items.append(iwrap)
+    gtype.options.append(iburger)
+    gtype.options.append(iwrap)
+    imain.ingredientgroups.append(gtype)
+    db.session.commit()
+
+    order = Order()
+    order.AddRootItem(imain.GetID(), 1)
+    order.AddIG("0.0", [iburger.GetID()], [1])
+    order.AddRootItem(imain.GetID(), 1)
+    with pytest.raises(ValueError):
+      order.AddIG("0.0", [iburger.GetID()], [1])
+      order.AddIG("1.0", [iburger.GetID()], [2])
+      order.AddIG("1.0", [], [])
+      order.AddIG("1.0", [iwrap.GetID()], [1])
