c2784f5e773ac2e6208e690e1a6a386bf5899dd6
Adam Yi <i@adamyi.com>
2019-04-04 13:58:14 +0000

    [PS9] core: implement order logic
    Change-Id: I66d444db18fe2bd9d27466977163335a2befb6a4

--- src/app/tests/test_user.py
+++ src/app/tests/test_user.py
@@ -8,7 +8,6 @@ def test_user_password():
   """
   user = User(email="test@foo.bar", user_type=0)
   user.SetPassword("test_pwd")
-  print(user.password)
   assert user.VerifyPassword("test_pwd")
   assert not user.VerifyPassword("wrong_pwd")
   assert not user.VerifyPassword(None)
