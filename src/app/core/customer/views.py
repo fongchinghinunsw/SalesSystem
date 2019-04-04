1979f8e8899856eb1e55ffe7b1bbf6f17986fbdb
Stephen Fong <fongchinghinunsw@gmail.com>
2019-04-04 12:38:11 +0000

    [PS1] customer: add order details page
    Change-Id: I502781cdc92c50f140ea5f225c08dce90f67c76e

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -1,9 +1,41 @@
 """Customer blueprint views"""
 
+import json
 from flask import render_template
+from app.core.models.order import Order
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
 
 @app.route("/")
-def home():
+def Home():
   return render_template('customer/landing.html')
+
+
+@app.route("/order/<oid>")
+def OrderDetailsPage(oid):
+  """This page shows the details of the order."""
+  order = Order.query.get(oid)
+  content = json.loads(order.content)
+  details = ""
+  for item in content:
+    details += GetDetailsString(item)
+
+  return render_template(
+      "customer/orderDetailsPage.html", order=order, details=details)
+
+
+def GetDetailsString(item, prefix=""):
+  """Recursively get the details string for an order item node
+  for invoice and order details"""
+  ret = ""
+  if item["type"] == "ig":
+    prefix += item["name"] + ":"
+    for child in item["options"]:
+      ret += GetDetailsString(child, prefix)
+  else:
+    ret = prefix + item["name"] + "\n"
+
+    prefix = (len(prefix) - len(prefix.lstrip()) + 2) * " "
+    for child in item["igs"]:
+      ret += GetDetailsString(child, prefix)
+  return ret
