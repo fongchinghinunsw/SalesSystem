7af178921b0c99d978a7d6a5de527592595dde01
Qingyi Zhang <2392506080@qq.com>
2019-04-06 08:17:25 +0000

    [PS11] admin: implement order list and tests
    Change-Id: I54dcfa6df92fac76b293212d4ba77bbfbd2de54b

--- src/app/core/admin/views.py
+++ src/app/core/admin/views.py
@@ -1,9 +1,16 @@
 """Admin blueprint views"""
 
 from flask import render_template
+from app.core.models.order import Order
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
 
 @app.route("/")
 def home():
   return render_template('admin/landing.html')
+
+
+@app.route("/orderlist", methods=['GET'])
+def order():
+  orders = Order.query.order_by(Order.updated_at.desc()).all()
+  return render_template("admin/order.html", orders=orders)
--- src/app/tests/test_admin.py
+++ src/app/tests/test_admin.py
@@ -1,5 +1,8 @@
 """Module to test the admin blueprint"""
 
+from app.core.models.order import Order
+from app.core.models import db
+
 
 def test_index(client):
   """ Test admin index
@@ -8,3 +11,36 @@ def test_index(client):
   """
   response = client.get('/admin/')
   assert b"Hello this is admin" in response.data
+
+
+def test_order_list(client, app):
+  """test order list page"""
+  with app.app_context():
+    order = Order(price=123, status=321)
+    db.session.add(order)
+    db.session.commit()
+    timestamp = order.GetCreatedAt()
+
+  response = client.get('/admin/orderlist')
+  rsp = str(response.data)
+  assert "ID" in rsp
+  assert "User Name" in rsp
+  assert "Price" in rsp
+  assert "Created at" in rsp
+  assert "Updated at" in rsp
+  assert "Status" in rsp
+  assert "123" in rsp
+  assert "321" in rsp
+  assert str(timestamp) in rsp
+
+  with app.app_context():
+    order1 = Order(price=231, status=222)
+    db.session.add(order1)
+    db.session.commit()
+
+  response = client.get('/admin/orderlist')
+  rsp = str(response.data)
+  assert "231" in rsp
+  position = rsp.find("123")
+  position1 = rsp.find("231")
+  assert position1 < position
