b8fce1788a922ee1a60372fd544669770e55db2e
Adam Yi <i@adamyi.com>
2019-04-07 01:06:06 +1100

    customer: add IG configuration frontend
    Change-Id: I92a8bb2bee62cbf5d29f23eeab3ca635cea9cba0

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -1,6 +1,7 @@
 """Customer blueprint views"""
 
-from flask import render_template
+from flask import render_template, request
+from app.core.models.inventory import IngredientGroup
 from app.core.models.order import Order
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
@@ -16,3 +17,27 @@ def OrderDetailsPage(oid):
   order = Order.query.get(oid)
 
   return render_template("customer/orderDetailsPage.html", order=order)
+
+
+@app.route("/order/<oid>/configure", methods=['GET', 'POST'])
+def IGConfPage(oid):
+  order = Order.query.get(oid)
+  if order.user.GetID() != session['uid']:
+    return "Access denied"
+
+  if request.method == 'POST':
+    path = request.form['path']
+    items = request.form.getlist('items')
+    numbers = request.form.getlist('numbers')
+    order.AddIG(path, items, numbers)
+    db.session.commit()
+
+  igdetails = order.GetUnfulfilledIGDetails()
+  if igdetails is None:
+    return redirect("/order/%d/menu" % oid)
+  ig = IngredientGroup.query.get(igdetails['id'])
+  return render_template(
+      "customer/ig.html",
+      ig=ig,
+      path=igdetails['path'],
+      item_name=igdetails['item_name'])
