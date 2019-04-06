1444d7042f80bb51c637983167b643d04aefcca2
Stephen Fong <fongchinghinunsw@gmail.com>
2019-04-06 09:03:23 +0000

    [PS6] customer: add order details page
    Change-Id: I502781cdc92c50f140ea5f225c08dce90f67c76e

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -1,9 +1,24 @@
 """Customer blueprint views"""
 
+import json
 from flask import render_template
+from app.core.models.order import Order
+from app.core.helpers.order import GetDetailsString
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
-
 @app.route("/")
-def home():
+def Home():
   return render_template('customer/landing.html')
+
+
+@app.route("/order/<oid>")
+def OrderDetailsPage(oid):
+  """This page shows the details of the order."""
+  order = Order.query.get(oid)
+  content = json.loads(order.content)
+  details = ""
+  for item in content:
+    details += GetDetailsString(item)
+
+  return render_template(
+      "customer/orderDetailsPage.html", order=order, details=details)
