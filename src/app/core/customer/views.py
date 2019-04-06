02420fba9eb4e9d2b62424499d1a3e1a633556fd
Qingyi Zhang <2392506080@qq.com>
2019-04-06 12:32:29 +0000

    [PS1] customer: vertify login and user type
    Change-Id: Ief75d5490ac298809ac413bf3218832fd2225aac

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -1,18 +1,22 @@
 """Customer blueprint views"""
 
-from flask import render_template
+from flask import render_template, session, redirect
 from app.core.models.order import Order
 from . import bp as app  # Note that app = blueprint, current_app = flask context
-
+from app.core.models.user import User
 
 @app.route("/")
 def Home():
   return render_template('customer/landing.html')
 
-
 @app.route("/order/<oid>")
 def OrderDetailsPage(oid):
   """This page shows the details of the order."""
   order = Order.query.get(oid)
+  if "uid" not in session:
+    return redirect("/accounts/signin")
+  user = User.query.get(session['uid'])
+  if user.GetType() == 0 and user.GetID() != order.user.GetID():
+    return "Access Denied!"
 
-  return render_template("customer/orderDetailsPage.html", order=order)
+  return render_template("customer/orderDetailsPage.html", order=order, usertype = user.GetType())
