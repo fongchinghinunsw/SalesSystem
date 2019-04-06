5d412757e2e1323b3444dd601c0b4857809e527a
Qingyi Zhang <2392506080@qq.com>
2019-04-06 12:31:47 +0000

    customer: add user authentication for order detail page
    Change-Id: Ief75d5490ac298809ac413bf3218832fd2225aac
    Reviewed-on: https://git.yiad.am/review/c/cs1531-mcdonalds/+/187
    Reviewed-by: Adam Yi <i@adamyi.com>
    Tested-by: Adam Yi <i@adamyi.com>

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -4,6 +4,7 @@ from flask import render_template, request, session, redirect, flash
 from app.core.models.inventory import IngredientGroup
 from app.core.models.order import Order
 from app.core.models.inventory import Item
+from app.core.models.user import User
 from app.core.models import db
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
@@ -17,8 +18,14 @@ def Home():
 def OrderDetailsPage(oid):
   """This page shows the details of the order."""
   order = Order.query.get(oid)
+  if "uid" not in session:
+    return redirect("/accounts/signin")
+  user = User.query.get(session['uid'])
+  if user.GetType() == 0 and user.GetID() != order.user.GetID():
+    return "Access Denied!"
 
-  return render_template("customer/orderDetailsPage.html", order=order)
+  return render_template(
+      "customer/orderDetailsPage.html", order=order, usertype=user.GetType())
 
 
 @app.route("/order")
