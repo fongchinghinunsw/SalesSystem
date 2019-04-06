805e86e10712fa731aa204332f9a9c04241a3d62
Adam Yi <i@adamyi.com>
2019-04-06 14:06:13 +0000

    [PS1] customer: add IG configuration frontend
    Change-Id: I92a8bb2bee62cbf5d29f23eeab3ca635cea9cba0

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -1,6 +1,7 @@
 """Customer blueprint views"""
 
-from flask import render_template
+from flask import render_template, request
+from app.core.models.inventory import IngredientGroup
 from app.core.models.order import Order
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
@@ -16,3 +17,26 @@ def OrderDetailsPage(oid):
   order = Order.query.get(oid)
 
   return render_template("customer/orderDetailsPage.html", order=order)
+
+
+@app.route("/order/<oid>/configure", methods=['GET', 'POST'])
+def IGConfPage(oid):
+  order = Order.query.get(oid)
+  #TODO(adamyi): check logged in user
+
+  if request.method == 'POST':
+    path = request.form['path']
+    items = request.form.getlist('items')
+    numbers = request.form.getlist('numbers')
+    order.AddIG(path, items, numbers)
+
+  igdetails = order.GetUnfulfilledIGDetails()
+  if igdetails is None:
+    #TODO(adamyi): redirect to menu for new root items
+    pass
+  ig = IngredientGroup.query.get(igdetails['id'])
+  return render_template(
+      "customer/ig.html",
+      ig=ig,
+      path=igdetails['path'],
+      item_name=igdetails['item_name'])
