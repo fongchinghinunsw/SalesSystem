1561ff09cf0f1eec6542c079c0ee07ef18744392
Qingyi Zhang <2392506080@qq.com>
2019-04-06 19:00:34 +0000

    [PS1] account: add mark as done button
    Change-Id: Iec12263456f444d290213e9fe5fd96ca332ba669

--- src/app/core/admin/views.py
+++ src/app/core/admin/views.py
@@ -1,16 +1,32 @@
 """Admin blueprint views"""
 
-from flask import render_template
+from flask import render_template, session, redirect
 from app.core.models.order import Order
+from app.core.models.user import User
+from app.core.models import db
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
 
 @app.route("/")
-def home():
+def Home():
   return render_template('admin/landing.html')
 
 
 @app.route("/orderlist")
-def order():
-  orders = Order.query.order_by(Order.updated_at.desc()).all()
-  return render_template("admin/order.html", orders=orders)
+def OrderList():
+  user = User.query.get(session['uid'])
+  if user.GetType() == 1:
+    orders = Order.query.order_by(Order.updated_at.desc()).all()
+    return render_template("admin/order.html", orders=orders)
+  return "Access denied"
+
+
+@app.route("/order/<oid>/done")
+def MarkOrder(oid):
+  user = User.query.get(session['uid'])
+  if user.GetType() == 1:
+    order = Order.query.get(oid)
+    order.SetStatus(2)
+    db.session.commit()
+    return redirect("/order/%d" % order.GetID())
+  return "Access denied"
