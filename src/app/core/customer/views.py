ad0864df5c510be1c501f23878c0817b956fe487
Stephen <fongchinghinunsw@gmail.com>
2019-04-07 01:44:55 +1100

    customer: [UNTESTED] add order menu page
    Change-Id: Iedd3b0eaa601429cc4f1f28ecd4999dc71340433
    Reviewed-on: https://git.yiad.am/review/c/cs1531-mcdonalds/+/191
    Reviewed-by: Adam Yi <i@adamyi.com>
    Tested-by: Adam Yi <i@adamyi.com>

--- src/app/core/customer/views.py
+++ src/app/core/customer/views.py
@@ -3,6 +3,7 @@
 from flask import render_template, request
 from app.core.models.inventory import IngredientGroup
 from app.core.models.order import Order
+from app.core.models.inventory import Item
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
 
@@ -17,6 +18,33 @@ def OrderDetailsPage(oid):
   order = Order.query.get(oid)
 
   return render_template("customer/orderDetailsPage.html", order=order)
+
+
+@app.route("/order")
+def NewOrder():
+  order = Order(user_id=session['uid'], status=0)
+  db.session.add(order)
+  db.session.commit()
+  return redirect("/order/%d/menu" % order.GetID())
+
+
+@app.route("/order/<oid>/menu", methods=["GET", "POST"])
+def OrderMenuPage(oid):
+  """This page allows ordering the main."""
+  order = Order.query.get(oid)
+  if order.user.GetID() != session['uid']:
+    return "Access denied"
+
+  if request.method == "POST":
+    items = request.form.getlist('items')
+    numbers = request.form.getlist('numbers')
+    for idx, item in enumerate(items):
+      if numbers[idx] > 0:
+        order.AddRootItem(item, numbers[idx])
+    db.session.commit()
+    return redirect("/order/<oid>/configure" % order.id)
+  items = Item.query.filter(root=True).all()
+  return render_template("/customer/menu.html", items=items)
 
 
 @app.route("/order/<oid>/configure", methods=['GET', 'POST'])
