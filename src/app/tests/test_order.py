40b0dee4155ce0b896dd5522468c2503a5fc5496
Stephen Fong <fongchinghinunsw@gmail.com>
2019-04-07 13:29:35 +0000

    [PS2] order: add test pay order 2
    Change-Id: I84ec99cacb6917d4a85a7afded9003980d2ca4fb

--- src/app/tests/test_order.py
+++ src/app/tests/test_order.py
@@ -78,7 +78,7 @@ def test_order_details_string(app):
   type:wrap ......$3.30\n\n\nTotal price: $8.30"""
 
 
-def test_pay_order(app):
+def test_pay_order_1(app):
   """ Test pay for order
   """
   with app.app_context():
@@ -105,8 +105,10 @@ def test_pay_order(app):
     order = Order()
     order.AddRootItem(imain.GetID(), 1)
     order.AddIG("0.0", [iburger.GetID()], [1])
+    assert order.GetPrice() == 5
     order.AddRootItem(imain.GetID(), 1)
     order.AddIG("1.0", [iwrap.GetID()], [1])
+    assert order.GetPrice() == 8.3
     db.session.add(order)
     db.session.commit()
 
@@ -117,3 +119,76 @@ def test_pay_order(app):
     assert order.GetPrice() == 8.3
     assert sburger.GetAmount() == 2
     assert swrap.GetAmount() == 1
+
+def test_pay_order_2(app):
+  """ Test pay order with complicated types of food
+  """
+  with app.app_context():
+    sburger = Stock(name="burger", amount=3)
+    swrap = Stock(name="wrap", amount=3)
+    ssesamebun = Stock(name="sesamebun", amount=2)
+    snormalbun = Stock(name="normalbun", amount=6)
+
+    db.session.add(sburger)
+    db.session.add(swrap)
+    db.session.add(ssesamebun)
+    db.session.add(snormalbun)
+
+    imain = Item(name="main", price=0)
+    iburger = Item(name="burger", price=5)
+    iwrap = Item(name="wrap", price=3.3, stock_unit=2)
+    isesamebun = Item(name="sesamebun", price=2)
+    inormalbun = Item(name="normalbun", price=2)
+
+    gtype = IngredientGroup(
+        name="type", min_item=1, max_item=1, min_option=1, max_option=1)
+    gbun = IngredientGroup(
+        name="bun", min_item=2, max_item=3, min_option=1, max_option=3)
+    db.session.add(imain)
+    db.session.add(iburger)
+    db.session.add(iwrap)
+
+    db.session.add(isesamebun)
+    db.session.add(inormalbun)
+
+    db.session.add(gtype)
+    db.session.add(gbun)
+
+    sburger.items.append(iburger)
+    swrap.items.append(iwrap)
+
+    ssesamebun.items.append(isesamebun)
+    snormalbun.items.append(inormalbun)
+
+    gtype.options.append(iburger)
+    gtype.options.append(iwrap)
+
+    gbun.options.append(isesamebun)
+    gbun.options.append(inormalbun)
+
+    imain.ingredientgroups.append(gtype)
+    iburger.ingredientgroups.append(gbun)
+    db.session.commit()
+
+    order = Order()
+    order.AddRootItem(imain.GetID(), 1)
+    order.AddIG("0.0", [iburger.GetID()], [1])
+    assert order.GetPrice() == 5
+    order.AddRootItem(imain.GetID(), 1)
+    order.AddIG("1.0", [iwrap.GetID()], [1])
+    assert order.GetPrice() == 8.3
+
+    order.AddIG("0.0.0.0", [isesamebun.GetID()], [2])
+
+    db.session.add(order)
+    db.session.commit()
+
+    order.Pay()
+    db.session.commit()
+
+    assert order.GetStatus() == 1  # paid
+    assert order.GetPrice() == 12.3
+    assert sburger.GetAmount() == 2
+    assert swrap.GetAmount() == 1
+    assert ssesamebun.GetAmount() == 0
+    assert snormalbun.GetAmount() == 6
