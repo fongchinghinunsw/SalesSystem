246b247ff14351129c06098379e58feb35beb75a
Stephen <fongchinghinunsw@gmail.com>
2019-04-07 19:39:24 +1000

    customer: add checkout page unit test
    Change-Id: I92e347711f03cd036bc39ffcef1e014741360983
    Reviewed-on: https://git.yiad.am/review/c/cs1531-mcdonalds/+/204
    Reviewed-by: Adam Yi <i@adamyi.com>
    Tested-by: Adam Yi <i@adamyi.com>

--- src/app/tests/test_customer.py
+++ src/app/tests/test_customer.py
@@ -85,3 +85,51 @@ def test_order_details(client, app):
     login(client, "Superman@gmail.com", "123456")
     response = client.get('/order/%d' % order.GetID())
     assert b"Superman" in response.data
+
+    # Check if the user can see his unique order ID in the details page.
+    assert "ORDER {0}".format(order.GetID()).encode("utf-8") in response.data
+
+
+def test_checkout_page(client, app):
+  """ Test GetDetailsString in Order class
+  """
+  with app.app_context():
+    sburger = Stock(name="burger", amount=10)
+    swrap = Stock(name="wrap", amount=10)
+    db.session.add(sburger)
+    db.session.add(swrap)
+    imain = Item(name="main")
+    iburger = Item(name="burger")
+    iwrap = Item(name="wrap")
+    gtype = IngredientGroup(
+        name="type", min_item=1, max_item=1, min_option=1, max_option=1)
+    db.session.add(imain)
+    db.session.add(iburger)
+    db.session.add(iwrap)
+    db.session.add(gtype)
+    sburger.items.append(iburger)
+    swrap.items.append(iwrap)
+    gtype.options.append(iburger)
+    gtype.options.append(iwrap)
+    imain.ingredientgroups.append(gtype)
+    db.session.commit()
+    user = User(name="Dickson", email="dickon@gmail.com", user_type=1)
+    user.SetPassword("123456")
+    user2 = User(name="Superman", email="Superman@gmail.com", user_type=0)
+    user2.SetPassword("123456")
+    order = Order(status=1, price=100)
+    user2.orders.append(order)
+    order.AddRootItem(imain.GetID(), 1)
+    order.AddIG("0.0", [iburger.GetID()], [1])
+    db.session.add(user)
+    db.session.add(user2)
+    db.session.add(order)
+    db.session.commit()
+    login(client, "dickon@gmail.com", "123456")
+    order = Order.query.get(order.GetID())
+    response = client.get('/order/%d' % order.GetID())
+    # Check if the customer can see the unique ID for the order.
+    assert str(order.GetID()).encode("utf-8") in response.data
+    client.get('/admin/order/%d/done' % order.GetID())
+    # Check if the customer can see the details of his order.
+    assert order.GetDetailsString().encode("utf-8") in response.data
