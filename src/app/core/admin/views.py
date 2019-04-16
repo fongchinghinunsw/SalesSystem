fc9ff3afee9f3e8bd879b7168be5a8ee6e28ccca
Adam Yi <i@adamyi.com>
2019-04-16 13:17:02 +0000

    [PS1] admin: implement inventory list page
    Change-Id: I16d8ac00541c9d461285b9ef413f189d00ec439a

--- src/app/core/admin/views.py
+++ src/app/core/admin/views.py
@@ -3,6 +3,7 @@
 from flask import render_template, session, redirect, request, flash
 from app.core.models.order import Order, OrderStatus
 from app.core.models.user import User, UserType
+from app.core.models.inventory import Stock
 from app.core.models import db
 from . import bp as app  # Note that app = blueprint, current_app = flask context
 
@@ -12,6 +13,20 @@ def Home():
   return render_template('admin/landing.html')
 
 
+@app.route("/inventory")
+def Inventory():
+  """Display list of stock"""
+  if 'uid' not in session:
+    flash("Please sign in first", "error")
+    return redirect("/accounts/signin")
+  user = User.query.get(session['uid'])
+  if user.GetType() == UserType.CUSTOMER:
+    flash("Access denied", "error")
+    return redirect("/")
+  stocks = Stock.query.all()
+  return render_template("admin/inventory.html", stocks=stocks)
+
+
 @app.route("/orderlist")
 def OrderList():
   """Display list of current oders"""
@@ -19,12 +34,12 @@ def OrderList():
     flash("Please sign in first", "error")
     return redirect("/accounts/signin")
   user = User.query.get(session['uid'])
-  if user.GetType() == UserType.ADMIN:
-    orders = Order.query.filter(Order.status == OrderStatus.PAID).order_by(
-        Order.updated_at.desc()).all()
-    return render_template("admin/orderlist.html", orders=orders)
-  flash("Access denied", "error")
-  return redirect("/")
+  if user.GetType() == UserType.CUSTOMER:
+    flash("Access denied", "error")
+    return redirect("/")
+  orders = Order.query.filter(Order.status == OrderStatus.PAID).order_by(
+      Order.updated_at.desc()).all()
+  return render_template("admin/orderlist.html", orders=orders)
 
 
 @app.route("/order/<oid>/done")
@@ -34,13 +49,13 @@ def MarkOrder(oid):
     flash("Please sign in first", "error")
     return redirect("/accounts/signin")
   user = User.query.get(session['uid'])
-  if user.GetType() == UserType.ADMIN:
-    order = Order.query.get(oid)
-    order.SetStatus(OrderStatus.READY)
-    db.session.commit()
-    return redirect("/order/%d" % order.GetID())
-  flash("Access denied", "error")
-  return redirect("/")
+  if user.GetType() == UserType.CUSTOMER:
+    flash("Access denied", "error")
+    return redirect("/")
+  order = Order.query.get(oid)
+  order.SetStatus(OrderStatus.READY)
+  db.session.commit()
+  return redirect("/order/%d" % order.GetID())
 
 
 @app.route("/join", methods=["GET", "POST"])
