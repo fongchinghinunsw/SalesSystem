a511b6429a737d8d3a2d858b8895a400512f7002
Adam Yi <i@adamyi.com>
2019-04-17 02:43:07 +1000

    customer: bug fix
    This is to avoid the scenario where we have stock for 3 cokes, and
    the user orders 5 separete ones.
    
    In this case we won't be able to tell coke is out of stock.
    We can only tell customer that we are out of stock when he/she checks out.
    
    This CL should resolve this issue by displaying whether item is out of
    stock promptly
    
    Change-Id: I29d698255eff9924f47096d856d8db01e6d12628
    Reviewed-on: https://git.yiad.am/review/c/cs1531-mcdonalds/+/224
    Reviewed-by: Stephen Fong <fongchinghinunsw@gmail.com>
    Tested-by: Adam Yi <i@adamyi.com>

--- src/app/core/models/order.py
+++ src/app/core/models/order.py
@@ -115,12 +115,17 @@ class Order(db.Model):
         return ret
     return None
 
+  def DeductStock(self):
+    content = json.loads(self.content)
+    for item in content:
+      ItemNode.FromDict(item).DeductStock()
+
   def Pay(self):
     if self.GetUnfulfilledIGDetails() is not None:
       raise RuntimeError("Ingredient group configuration is not complete")
     content = json.loads(self.content)
     for item in content:
-      ItemNode.FromDict(item).Pay()
+      ItemNode.FromDict(item).DeductStock()
     self.status = OrderStatus.PAID
 
 
@@ -207,13 +212,16 @@ class ItemNode(OrderNode):
       ret += child.GetDetailsString(prefix)
     return ret
 
-  def Pay(self, coefficient=1):
+  def DeductStock(self, coefficient=1):
     item = Item.query.get(self.id)
-    if item.stock is not None:
-      item.stock.DecreaseAmount(item.stock_unit * self.num * coefficient)
+    try:
+      if item.stock is not None:
+        item.stock.DecreaseAmount(item.stock_unit * self.num * coefficient)
+    except RuntimeError:
+      raise RuntimeError("Stock not enough for %s" % self.name)
     coefficient *= self.num
     for child in self.children:
-      child.Pay(coefficient)
+      child.DeductStock(coefficient)
 
 
 class IGNode(OrderNode):
@@ -307,6 +315,6 @@ class IGNode(OrderNode):
       return {"path": path, "item_name": item_name, "id": self.id}
     return super().GetUnfulfilledIGDetails(path, item_name)
 
-  def Pay(self, coefficient):
+  def DeductStock(self, coefficient):
     for child in self.children:
-      child.Pay(coefficient)
+      child.DeductStock(coefficient)
